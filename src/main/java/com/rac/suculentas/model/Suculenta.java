5ed2e7d2c7dae425ca2d94a8eb3bca9470878b80
Alberto C <btocarmona@gmail.com>
2024-03-23 17:58:07 -0300

    Model Suculenta

---
+++ src/main/java/com/rac/suculentas/model/Suculenta.java
@@ -0,0 +1,28 @@
+package com.rac.suculentas.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+
+@Entity
+public class Suculenta {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid",strategy = "uuid2")
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private Categoria categoria;
+    private Double tamanio;
+    private String cuidados;
+    private String floracion;
+    private Double alturaMax;
+    private Boolean disponibilidad;
+    @Temporal(TemporalType.DATE)
+    private Date fechaAlta;
+    private Comentario comentario;
+}
