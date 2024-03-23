d5ac31be00bb2622e4ef53f937044fe974e96bcd
Alberto C <btocarmona@gmail.com>
2024-03-23 02:44:26 -0300

    Primer Commit

---
+++ src/main/java/com/rac/suculentas/SuculentasApplication.java
@@ -0,0 +1,13 @@
+package com.rac.suculentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuculentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuculentasApplication.class, args);
+    }
+
+}
